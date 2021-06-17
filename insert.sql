f040a24bc9891f05c2fc568e95d03d6fa68cfcc9
Ashish <geethaashish123@gmail.com>
2021-06-17 12:30:00 +0530

    turned on foreign keys

--- insert.sql
+++ insert.sql
@@ -1,3 +1,7 @@
+
+PRAGMA foreign_keys = ON;
+
+
 insert into publisher (name , country) values ( 'PHI' , 'India');
 insert into publisher (name , country) values ( 'Harper' , 'USA');
 insert into publisher (name , country) values ( 'GCP' , 'USA');
